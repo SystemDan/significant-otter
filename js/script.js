e7d82c51e1619130c57810805e1d9bceb042a0a2
Paul Hansa <bigcooki3@users.noreply.github.com>
2018-10-06 03:24:24 -0400

    Add otter usernames

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@ $(document).ready(function () {
     var i = Math.floor(Math.random() * 10) + 1;
     path = '../img/' + i + '.jpg';
     $('#otter_img').attr('src', path);
+    $('#upvote').attr('href', path);
+
+    let names = [
+        'Harry-Otter',
+        'OttermanEmpire14',
+        'xXx_OtterOfDarkness_xXx',
+        '<3OtterlyAdorable<3',
+        'otteryoutalkingabout',
+        'otternal_lyfe420',
+        'AceOtterney',
+        'OtterworldlyHalfPrince',
+        'GrandTh3ftOtter',
+        'not_any_otter_guy'
+    ];
+    $('#otter_name').text(names[i - 1]);
+    console.log('success');
 });
